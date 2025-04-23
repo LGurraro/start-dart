daff50c859666958d080df3330626797b4092215
Livia Gurraro <livia-gu@hotmail.com>
2025-04-23 00:59:42 -0300

    para.dart

---
+++ input/logica_programacao/lib/Condicionais/estrutura_repeticao/para/para.dart
@@ -0,0 +1,15 @@
+
+
+
+
+void main(){
+
+// Estrutura do Para (For)
+// for (inicialização ou valor inicial; condição; incremento) 
+
+for(var contador =1; contador <=5; contador++){
+  print('Bom dia!');
+
+}
+print('Fim do programa');
+}
