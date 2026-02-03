b97773d1eb0b52825cb4ae21c0ecf0e098614c93
Livia Gurraro <livia.gurarro@hotmail.com>
2026-02-03 16:02:37 -0300

    pilha.dart`

---
+++ input/logica_programacao/lib/funcao/pilha.dart
@@ -0,0 +1,16 @@
+void main() {
+  print('Iniciando');
+  fun1();
+}
+void fun1(){
+  fun3();
+}
+void fun2(){
+  fun4();
+}
+void fun3(){
+  fun2();
+}
+void fun4() {
+  print(StackTrace.current);
+}
